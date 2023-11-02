ac553075432028cfa82c82f32db95bc9bdbeb9bc
Nirmal Bhandari <vhandarinirmal@gmail.com>
2023-11-01 20:13:27 -0500

    login form bootstrap

--- a3s/src/App.js
+++ a3s/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+// import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+
+// import Preferences from './components/Preferences/Preferences';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Login Page</h1>
+        {/* <Dashboard/>
+        <Preferences/> */}
+        <Login/>
     </div>
   );
 }
---
+++ a3s/src/components/Preferences/Preferences.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Preferences extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Preferences</h2>
+      </div>
+    )
+  }
+}
+
+export default Preferences
